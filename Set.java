1d90b59a88f0a357a8d0f304ecf9b77da61a6271
Petru Gurita <petru.gurita1@gmail.com>
2018-03-11 16:14:55 +0200

    thread safe - working environment

--- Set.java
+++ Set.java
@@ -1,21 +1,29 @@
-package cdl;
-
 public class Set implements Commands{
 
 	private int registerIndex;
-	private int value;
+	private int valueOrRegisterIndex;
+	private boolean isRegisterIndex;
 	
-	public Set(int registerIndex, int value) {
+	public Set(String valueOrRegisterIndex, int registerIndex) {
+		this.isRegisterIndex = false;
 		this.registerIndex = registerIndex;
-		this.value = value;
+		if (valueOrRegisterIndex.charAt(0) == 'R') {
+			this.isRegisterIndex = true;
+			this.valueOrRegisterIndex = Integer.parseInt(valueOrRegisterIndex.substring(1));
+		} else {
+			this.valueOrRegisterIndex = Integer.parseInt(valueOrRegisterIndex);
+		}
 	}
 
 	@Override
 	public void execute(Process p) {
 		// TODO Auto-generated method stub
 		if (registerIndex > 31 || registerIndex < 0) return;
-		
-		p.setRegisterValue(registerIndex, value);
+		int newSetValue = valueOrRegisterIndex;
+		if (isRegisterIndex == true) {
+			newSetValue = p.getRegisterValue(valueOrRegisterIndex);
+		}
+		p.setRegisterValue(registerIndex, newSetValue);
 	}
 	
 }
